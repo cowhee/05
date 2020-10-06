cd6781a76bbcc8003fa4a5c107451fa153b87295
cowhee <71056160+cowhee@users.noreply.github.com>
2020-10-06 15:33:05 +0900

    01

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	
+	printf("정수를 입력하시오.:"); 
+	scanf("%d", &n);
+	 
+	if(n>0){
+		printf("%d는양수\n", n);
+	}
+	else if(n==0){
+		printf("%d는0\n", n);
+	}
+	else {
+		printf("%d는음수\n", n);
+	}
+	return 0;
+}
